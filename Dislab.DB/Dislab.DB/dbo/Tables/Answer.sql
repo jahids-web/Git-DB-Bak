0de4d7351622cdcd635d14fd2a9d316c6ad527cd
jahids-web <jahiduddin922@gmail.com>
2024-02-21 12:45:41 +0600

    Dislab.DB

---
+++ Dislab.DB/Dislab.DB/dbo/Tables/Answer.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Answer] (
+    [Id]         BIGINT         IDENTITY (1, 1) NOT NULL,
+    [QuestionId] BIGINT         NOT NULL,
+    [AnswerBody] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Answer] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Dislab.DB/Dislab.DB/dbo/Tables/Question.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Question] (
+    [Id]            BIGINT         IDENTITY (1, 1) NOT NULL,
+    [QuestionTitle] NVARCHAR (MAX) NOT NULL,
+    [QuestionBody]  NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_AskQuestion] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
